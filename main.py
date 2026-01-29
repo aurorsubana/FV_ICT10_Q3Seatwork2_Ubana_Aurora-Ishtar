e79636aaf800dd2f97f5ee8d38f37632c80d46c0
aurora <ubana.aa@obmontessori.edu.ph>
2026-01-29 10:59:41 +0800

    Refactor checkTeamStatus and clearStatus functions

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ def checkTeamStatus():
         medicalCert = document.getElementById('medicalCert').checked
         
         if registered and medicalCert:
-            resultDiv.innerHTML = 'you are in red bulldogs, you are now fully able 2 participate! ( ´ ꒳ ` ) ♡'
+            resultDiv.innerHTML = 'you are in red bulldogs, you are now fully able 2 participate! ( ´ ꒳ ` ) ♡' #when both boxes r checked
         elif registered:
             resultDiv.innerHTML = 'you are in red bulldogs, you are registered but you dont have a medical certificate (￣ ￣|||)'
         elif medicalCert:
             resultDiv.innerHTML = 'you are in red bulldogs, but you are not registered...`(°ー°〃)'
         else:
-            resultDiv.innerHTML = 'you are not able to participate!(＃`Д´)'
+            resultDiv.innerHTML = 'you are not able to participate!(＃`Д´)' #these r here so that when the user doesnt check any of the boxes this shows up
     elif selectedTeam == 'Team B':
         registered = document.getElementById('registered').checked
         medicalCert = document.getElementById('medicalCert').checked
@@ -24,7 +24,7 @@ def checkTeamStatus():
         if registered and medicalCert:
             resultDiv.innerHTML = 'you are in blue bears, you are now fully able 2 participate! ( ´ ꒳ ` ) ♡'
         elif registered:
-            resultDiv.innerHTML = 'you are in blue bears, you are registered but you dont have a medical certificate (￣ ￣|||)'
+            resultDiv.innerHTML = 'you are in blue bears, you are registered but you dont have a medical certificate (￣ ￣|||)' #when only the registered box has only been checked
         elif medicalCert:
             resultDiv.innerHTML = 'you are in blue bears, but you are not registered...`(°ー°〃)'
         else:
@@ -38,7 +38,7 @@ def checkTeamStatus():
         elif registered:
             resultDiv.innerHTML = 'you are in green hornets, you are registered but you dont have a medical certificate (￣ ￣|||)'
         elif medicalCert:
-            resultDiv.innerHTML = 'you are in green hornets, but you are not registered...`(°ー°〃)'
+            resultDiv.innerHTML = 'you are in green hornets, but you are not registered...`(°ー°〃)' #when the medical certificate box has only been checked
         else:
             resultDiv.innerHTML = 'you are not able to participate!(＃`Д´)'
     elif selectedTeam == 'Team D':
@@ -54,7 +54,7 @@ def checkTeamStatus():
         else:
             resultDiv.innerHTML = 'you are not able to participate!(＃`Д´)'
     else:
-        resultDiv.innerHTML = 'select a team plz'
+        resultDiv.innerHTML = 'select a team plz' #when they havent done anything and just pressed the check button...
 
 def clearStatus():
     document.getElementById('registered').checked = False
@@ -63,4 +63,5 @@ def clearStatus():
     document.getElementById('result').innerHTML = ''
 
 window.checkTeamStatus = checkTeamStatus
+
 window.clearStatus = clearStatus
