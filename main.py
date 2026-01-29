50410c3b58ab1a050906aa8cd14c16a26df660e6
aurora <ubana.aa@obmontessori.edu.ph>
2026-01-29 10:53:55 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,66 @@
+from pyscript import document, window
+
+def checkTeamStatus():
+    teamSelect = document.getElementById('selectTeam')
+    selectedTeam = teamSelect.value
+    resultDiv = document.getElementById('result')
+    
+    if selectedTeam == 'Team A':
+        registered = document.getElementById('registered').checked
+        medicalCert = document.getElementById('medicalCert').checked
+        
+        if registered and medicalCert:
+            resultDiv.innerHTML = 'you are in red bulldogs, you are now fully able 2 participate! ( ´ ꒳ ` ) ♡'
+        elif registered:
+            resultDiv.innerHTML = 'you are in red bulldogs, you are registered but you dont have a medical certificate (￣ ￣|||)'
+        elif medicalCert:
+            resultDiv.innerHTML = 'you are in red bulldogs, but you are not registered...`(°ー°〃)'
+        else:
+            resultDiv.innerHTML = 'you are not able to participate!(＃`Д´)'
+    elif selectedTeam == 'Team B':
+        registered = document.getElementById('registered').checked
+        medicalCert = document.getElementById('medicalCert').checked
+        
+        if registered and medicalCert:
+            resultDiv.innerHTML = 'you are in blue bears, you are now fully able 2 participate! ( ´ ꒳ ` ) ♡'
+        elif registered:
+            resultDiv.innerHTML = 'you are in blue bears, you are registered but you dont have a medical certificate (￣ ￣|||)'
+        elif medicalCert:
+            resultDiv.innerHTML = 'you are in blue bears, but you are not registered...`(°ー°〃)'
+        else:
+            resultDiv.innerHTML = 'you are not able to participate!(＃`Д´)'
+    elif selectedTeam == 'Team C':
+        registered = document.getElementById('registered').checked
+        medicalCert = document.getElementById('medicalCert').checked
+        
+        if registered and medicalCert:
+            resultDiv.innerHTML = 'you are in green hornets, you are now fully able 2 participate! ( ´ ꒳ ` ) ♡'
+        elif registered:
+            resultDiv.innerHTML = 'you are in green hornets, you are registered but you dont have a medical certificate (￣ ￣|||)'
+        elif medicalCert:
+            resultDiv.innerHTML = 'you are in green hornets, but you are not registered...`(°ー°〃)'
+        else:
+            resultDiv.innerHTML = 'you are not able to participate!(＃`Д´)'
+    elif selectedTeam == 'Team D':
+        registered = document.getElementById('registered').checked
+        medicalCert = document.getElementById('medicalCert').checked
+        
+        if registered and medicalCert:
+            resultDiv.innerHTML = 'you are in yellow tigers, you are now fully able 2 participate! ( ´ ꒳ ` ) ♡'
+        elif registered:
+            resultDiv.innerHTML = 'you are in yellow tigers, you are registered but you dont have a medical certificate (￣ ￣|||)'
+        elif medicalCert:
+            resultDiv.innerHTML = 'you are in yellow tigers, but you are not registered...`(°ー°〃)'
+        else:
+            resultDiv.innerHTML = 'you are not able to participate!(＃`Д´)'
+    else:
+        resultDiv.innerHTML = 'select a team plz'
+
+def clearStatus():
+    document.getElementById('registered').checked = False
+    document.getElementById('medicalCert').checked = False
+    document.getElementById('selectTeam').value = ''
+    document.getElementById('result').innerHTML = ''
+
+window.checkTeamStatus = checkTeamStatus
+window.clearStatus = clearStatus
